549cc7c3afd38e327deb275469bf275f3b10aeb0
42-Ceasure <cglavieu@student.42.fr>
2024-11-15 18:59:41 +0100

    add realloc first untested throw

--- proj/src/realloc.c
+++ proj/src/realloc.c
@@ -12,28 +12,42 @@
 
 #include <mymalloc.h>
 
-void	*realloc(void *ptr, size_t size)
+void	*realloc(void *ptr, size_t usr_size)
 {
-	/*
+	void	*tmp;
+	void	*chunk_ptr;
+	size_t	usr_data_size;
+	size_t	usr_chunk_size;	
+	size_t	size;
 
-	check le pointeur,
-		si invalide, return;
-		si NULL, faire un mymalloc(size);
-
-	check la size,
-		si 0 et ptr non null, faire free(ptr);
-		si size < taille du chunk, allouer size
-		si size > taille du chunk
-		regarder bloc d'apres
-			s'il suffit, merge blocs, allouer size
-			s'il ne suffit pas, fffit
-			copier
-			free precedent
-	
-	retourner pointeur
-
-	*/
-	(void)ptr; 
-	(void)size;
+	if (ptr == NULL)
+		return (mymalloc(usr_size));
+	else if (size == 0)
+		myfree(ptr);
+	chunk_ptr = get_user_chunk(ptr);
+	if (chunk_ptr == NULL)
+		return (NULL); // invalid pointer
+	size = set_chunk_size(usr_size);
+	usr_chunk_size = get_chunk_size(chunk_ptr);
+	usr_data_size = usr_chunk_size - DATA_SIZE;
+	if (size < usr_chunk_size)
+		allocate(chunk_ptr, size);
+	else if (size > usr_chunk_size)
+	{
+		tmp = next_chunk(chunk_ptr);
+		if (!is_chunk_used(tmp) && size < usr_chunk_size + get_chunk_size(tmp))
+		{
+			merge_chunks(chunk_ptr, tmp, 0);
+			allocate(chunk_ptr, size);
+			return (ptr);
+		}
+		else
+		{
+			tmp = mymalloc(usr_size);
+			memcpy(usrptr_from_chkptr(tmp), ptr, usr_data_size);
+			myfree(ptr);
+			return (tmp);
+		}
+	}
 	return (NULL);
 }
