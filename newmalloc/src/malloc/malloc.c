1ff6d6551681301c2551cae8b59d7d63f878dc6f
42-Ceasure <cglavieu@student.42.fr>
2025-02-13 07:34:33 +0100

    urgence push deplacement

---
+++ newmalloc/src/malloc/malloc.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   malloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cglavieu <cglavieu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 14:01:09 by cglavieu          #+#    #+#             */
+/*   Updated: 2025/02/10 13:35:01 by cglavieu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# define TINY_SIZE		(64)
+# define MEDIUM_SIZE	(256)
+
+# define ALIGNMENT		(2 * sizeof(size_t))
+# define HEAD_SIZE		(sizeof(size_t))
+# define FOOT_SIZE		(HEAD_SIZE)
+# define DATA_SIZE		(HEAD_SIZE + FOOT_SIZE)
+
+typedef enum	e_alloc_type
+{
+	TINY,
+	MEDIUM,
+	LARGE
+}				t_type;
+
+void	*chunk_manager(void *page, size_t size)
+{
+	void	*zone;
+	/*
+	
+	*/
+	return (zone);
+}
+
+void	*page_manager(t_type type, size_t size)
+{
+	void	*page;
+	void	*root;
+
+	page = NULL;
+	if (!is_type_inited(type))
+		set_type_root(type);
+	root = get_type_root(type);
+	page = find_fit_page(root, size);
+	if (!page)
+		page = extend_root(root);
+	if (!page)
+		return (NULL);
+	return (page);
+}
+
+t_type	type_manager(size_t size)
+{
+	t_type type;
+
+	if (size <= TINY_SIZE)
+		type = TINY;
+	else if (size <= MEDIUM_SIZE)
+		type = MEDIUM;
+	else
+		type = LARGE;
+	return (type);	
+}
+
+size_t	size_manager(const size_t user_size)
+{
+	size_t	padding;
+
+	padding = (ALIGNMENT - (user_size % ALIGNMENT));
+	if (padding == ALIGNMENT)
+		padding = 0;
+	return (user_size + padding + DATA_SIZE);
+}
+
+void	*my_malloc(size_t user_size)
+{
+	size_t	size;
+	t_type	type;
+	void	*page;
+	void	*zone;
+
+	if (user_size < 1)
+		return (NULL);
+	size = size_manager(user_size);
+	type = type_manager(size);
+	page = page_manager(type, size);
+	if (page == NULL)
+		return (NULL);
+	zone = chunk_manager(page, size);
+	if (zone == NULL)
+		return (NULL);
+	return (zone);
+}
+
+int		main(void)
+{
+	void	*ptr;
+	size_t	x;
+
+	ptr = mymalloc(x);
+}
---
+++ newmalloc/src/page/page.c
@@ -0,0 +1,207 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   page.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cglavieu <cglavieu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/24 16:03:32 by cglavieu          #+#    #+#             */
+/*   Updated: 2025/02/12 14:54:01 by cglavieu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# define PAGE_SIZE		(getpagesize())
+# define PAGE_ELEMENTS	(8)
+# define PAGE_HEADSIZE	(PAGE_ELEMENTS * sizeof(size_t))
+
+# define DEFAULT_TINY	(2)
+# define DEFAULT_MEDIUM	(8)
+# define DEFAULT_LARGE	(1)
+
+# define ID				0
+# define NEXTPAGE		1
+# define PREVPAGE		2
+# define LASTPAGE		3
+# define ALLOCATIONS	4
+# define MAXALLOCABLE	5
+# define STARTADDR		6
+# define STOPADDR		7
+# define START			8
+
+
+typedef	struct	s_heap
+{
+	void	*tiny;
+	void	*medium;
+	void	*large;
+}				t_heap;
+
+t_heap *g_heap = NULL;
+
+void	*getmap(const size_t len)
+{
+	void	*map;
+
+	map = mmap(	NULL,
+				len,
+				PROT_READ | PROT_WRITE,
+				MAP_PRIVATE | MAP_ANONYMOUS,
+				-1,
+				0);
+	return (map);
+}
+
+void	init_heap(void)
+{
+	static	t_heap heap;
+
+	g_heap = &heap;
+	g_heap->tiny = NULL;
+	g_heap->medium = NULL;
+	g_heap->large = NULL;
+}
+
+int		is_type_inited(t_type type)
+{
+	if (g_heap == NULL)
+	{
+		init_heap();
+		return (0);
+	}
+	switch (type)
+	{
+		case TINY:
+			if (g_heap->tiny == NULL)
+				return (0);
+		case MEDIUM:
+			if (g_heap->medium == NULL)
+				return (0);
+		case LARGE:
+			if (g_heap->large == NULL)
+				return (0);
+	}
+	return (1);
+}
+
+void	set_page_id(size_t *page, size_t id)
+{
+	*(page + ID) = id;
+}
+void	set_page_nxtp(size_t *page, size_t *nextpage)
+{
+	write_adress((page + NEXTPAGE), nextpage);
+}
+void	set_page_prvp(size_t *page, size_t *prevpage)
+{
+	write_adress((page + PREVPAGE), prevpage);
+}
+void	set_page_endp(size_t *page, size_t *lastpage)
+{
+	write_adress((page + LASTPAGE), lastpage);
+}
+void	set_page_alloc(size_t *page, size_t allocations)
+{
+	*(page + ALLOCATIONS) = allocations;
+}
+void	set_page_max(size_t *page, size_t maxallocable)
+{
+	*(page + MAXALLOCABLE) = maxallocable;
+}
+void	set_page_start(size_t *page, size_t *startaddr)
+{
+	write_adress((page + STARTADDR), startaddr);
+}
+void	set_page_stop(size_t *page, size_t *stopaddr)
+{
+	write_adress((page + STOPADDR), stopaddr);
+}
+
+
+size_t	get_page_id(size_t *page, size_t id)
+{
+	return (*(page + ID));
+}
+void	*get_page_nxtp(size_t *page, size_t *nextpage)
+{
+	return (get_adress(page + NEXTPAGE));
+}
+void	*get_page_prvp(size_t *page, size_t *prevpage)
+{
+	return (get_adress(page + NEXTPAGE));
+}
+void	*get_page_endp(size_t *page, size_t *lastpage)
+{
+
+}
+size_t	get_page_alloc(size_t *page, size_t allocations)
+{
+
+}
+size_t	get_page_max(size_t *page, size_t maxallocable)
+{
+
+}
+void	*get_page_start(size_t *page, size_t *startaddr)
+{
+
+}
+void	*get_page_stop(size_t *page, size_t *stopaddr)
+{
+
+}
+
+void	init_page_header(void *page, size_t size)
+{
+	static size_t	id = 0;
+
+	set_page_id(size_t *page, id);
+	set_page_nxtp(size_t *page, NULL);
+	set_page_prvp(size_t *page, NULL);
+	set_page_endp(size_t *page, NULL);
+	set_page_alloc(size_t *page, 0);
+	set_page_max(size_t *page, size - PAGE_HEADSIZE);
+	set_page_start(size_t *page, page + START);
+	set_page_stop(size_t *page, page + size);
+	id++;
+}
+
+void	*new_page(size_t nb)
+{
+	void	*page;
+	size_t	size;
+
+	size = nb * PAGE_SIZE;
+	page = getmap(size);
+	init_page_header(page, size);
+	return (page);
+}
+
+void	set_type_root(t_type type)
+{
+	switch (type)
+	{
+		case TINY:
+			g_heap->tiny = new_page(DEFAULT_TINY);
+		case MEDIUM:
+			g_heap->medium = new_page(DEFAULT_MEDIUM);
+		case LARGE:
+			g_heap->large = new_page(DEFAULT_LARGE);
+	}
+}
+
+void	*get_type_root(t_type type)
+{
+	switch (type)
+	{
+		case TINY:
+			return (g_heap->tiny);
+		case MEDIUM:
+			return (g_heap->medium);
+		case LARGE:
+			return (g_heap->large);
+	}
+}
+
+void	*find_fit_page(void *page, size_t size);
+
+void	extend_root(void *page);
